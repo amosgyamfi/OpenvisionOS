cb94724f549fca0d377bf11fd94ef27fe9cc9751
Amos Gyamfi <amosgyamfi@Amoss-Mac-mini.local>
2023-07-23 09:15:47 +0300

    Adds Xcode project

---
+++ OpenvisionOSProject/OpenvisionOSProject/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  OpenvisionOSProject
+//
+//  Created by Amos Gyamfi on 23.7.2023.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenvisionOSProject/OpenvisionOSProject/OpenvisionOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenvisionOSProjectApp.swift
+//  OpenvisionOSProject
+//
+//  Created by Amos Gyamfi on 23.7.2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenvisionOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
